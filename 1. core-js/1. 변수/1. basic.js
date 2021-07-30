17666a129e3ee4f05a55d075f376a1b4a7718ddb
kokokaki <hsg9984@gmail.com>
2021-07-30 15:41:18 +0900

    [21.07.30] # 자바스크립트 변수, 데이터 타입(숫자, 문자열) 학습

---
+++ 1. core-js/1. 변수/1. basic.js
@@ -0,0 +1,33 @@
+
+
+//특정 연산의 결과를 저장하고 싶다.
+
+//1. 변수를 선언 (만든다)
+var result;
+
+//2. 변수를 초기화 (값을 저장)
+result = 10 + 20; // = : assignment(대입)
+ 
+
+console.log(result * 3);
+
+var haha;
+console.log(haha);
+
+
+//변수 선언과 함께 초기화 가능
+var total = result + 3;
+console.log(total);
+
+//변수의 재할당
+total = result * 3;
+console.log(total);
+
+//변수의 값 복사
+var copyTotal = total;
+
+total = total + 10;
+
+console.log(total);
+console.log(copyTotal);
+
---
+++ 1. core-js/2. 데이터 타입/2. 문자열타입.js
@@ -0,0 +1,40 @@
+
+var greeting;
+greeting = 'hello';
+greeting = "안녕ㅎㅎ";
+greeting = `잘가~`;
+
+console.log(typeof greeting);
+
+var str = '그는 나에게 "안녕"이라고 말했다.';
+console.log(str);
+
+str = "Let's Go!";
+console.log(str);
+
+str = "그는 나에게 \"안녕\"이라고 말했다.";
+
+//탈출 문자
+str = '나는 \t\t 홍길동 \n입니다.';
+console.log(str);
+
+var filePath = 'D:\\temp\\new_icon.png';
+console.log(filePath);
+
+
+//템플릿 리터럴 (ES6: 2015)
+var lyrics = "링딩동 링딩동 링디기딩기디링딩동~~~~~\n링딩동 링딩동 링디기딩기디링딩동~~~~~\n링딩동 링딩동 링디기딩기디링딩동~~~~~\n링딩동 링딩동 링디기딩기디링딩동~~~~~";
+// console.log(lyrics);
+
+lyrics = `링딩동 링딩동 링디기딩기디링딩동~~~~~
+링딩동 링딩동 링디기딩기디링딩동~~~~~
+링딩동 링딩동 링디기딩기디링딩동~~~~~
+링딩동 링딩동 링디기딩기디링딩동~~~~~`;
+console.log(lyrics);
+
+var month = 12;
+var day = 25;
+var anni = '크리스마스';
+
+console.log(month + '월 ' + day + '일은 ' + anni + '입니다.');
+console.log(`${month}월 ${day}일은 ${anni}입니다.`);
