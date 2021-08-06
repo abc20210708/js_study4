a2c10163f8853dcf951686b574fce94e7503732f
kokokaki <hsg9984@gmail.com>
2021-08-06 15:59:39 +0900

    [21.08.06] # 탈출문, 종합문제 학습

---
+++ 1. core-js/4. 제어문/3. 탈출문/0. 문제.js
@@ -0,0 +1,16 @@
+
+
+// 100보다 큰 수를 입력할 때까지 입력창띄우기
+
+
+while (true) {
+    var number = +prompt('100보다 큰 수를 입력해라~');
+
+    if (number > 100) {
+        alert('참 잘했어요');
+        break;
+    }
+}
+
+
+
